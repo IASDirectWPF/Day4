eedcacd9cdd0daea0a2a1983bc49c3bd86e3142b
Jeff McBride <jeff@geeklearn.com>
2019-01-30 22:31:36 -0600

    Day4 Class Files Added

---
+++ Module 11 Data Binding/Code/Begin/Northwinder/NW.WPF/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NW.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show(
+                String.Format("# of Command Line Arguments = {0}", e.Args.Count()));
+        }
+    }
+
+}
